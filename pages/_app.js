919ec79999a887d92ecfae0c8fb15ecb66db6232
VeronicaSotoF <sotofierroveronicadev@gmail.com>
2022-08-19 12:37:34 -0500

    commit de prueba

--- pages/_app.js
+++ pages/_app.js
@@ -47,7 +47,7 @@ border: 1px solid rgba( 255, 255, 255, 0.18 );
   --accent-color: #A78556;
 }
 `;
-
+//imagenes alternativas
 //https://images.unsplash.com/photo-1576481564650-61d2ed81f6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80
 //https://images.unsplash.com/photo-1521512233585-c0bb78faee37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80
 
